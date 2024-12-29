73385122e5a7eb67ea39c7b87a8b4f1e3aa1cde8
evansmwenda <evansmwenda.em@gmail.com>
2024-12-29 20:58:43 +0300

    ongoing mpesa logic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MpesaSTKPushController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/v1/mpesatest/stk/push', [MpesaSTKPushController::class, 'STKPush']);
+
