c838abd9d4872bb075b230060ead04b879281d53
evansmwenda <evansmwenda.em@gmail.com>
2025-01-16 12:14:06 +0300

    added STK logic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MpesaSTKPushController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/v1/mpesatest/stk/push', [MpesaSTKPushController::class, 'STKPush']);
